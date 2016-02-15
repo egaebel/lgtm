0228e44a048f165cc83e6bb0f3fa2d30f5c38389
Ethan Gaebel <egaebel@vt.edu>
2016-02-15 15:09:17 -0500

    changed set-5300-wifi.sh to be run from csi-code instead of /lib/firmware

--- csi-code/set-5300-wifi.sh
+++ csi-code/set-5300-wifi.sh
@@ -1,1 +1,9 @@
-/lib/firmware/set-5300-wifi.sh
+#/bin/bash
+
+cd /lib/firmware
+sudo rm /lib/firmware/iwlwifi-5000-2.ucode
+sudo ln -s /lib/firmware/iwlwifi-5000-2.ucode.sigcomm2010 /lib/firmware/iwlwifi-5000-2.ucode
+sudo modprobe -r iwlwifi mac80211
+sleep 2
+sudo modprobe iwlwifi connector_log=0x1
+sudo killall wpa_supplicant
