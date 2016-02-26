85c81f36997fc22639e114e23cc51c40989acde6
Ethan Gaebel <egaebel@vt.edu>
2016-02-26 15:10:31 -0500

    updated script to 'switch back to regular internet'

--- wireless-adapter-configuration/switch-back-to-regular-internet.sh
+++ wireless-adapter-configuration/switch-back-to-regular-internet.sh
@@ -1,5 +1,11 @@
-#!/bin/bash
+#!/usr/bin/sudo /bin/bash
 
-sudo ifmetric wlan0 0
-sudo ifmetric wlan1 10
-sudo ifmetric wlan2 10
+#sudo ifmetric wlan0 0
+#sudo ifmetric wlan1 10
+#sudo ifmetric wlan2 10
+
+# The three lines below seem to be unncessary....killall wpa_supplicant seems to do the trick -_-
+#sudo modprobe iwlwifi mac80211 cfg80211
+#sudo modprobe iwlwifi mac80211 cfg80211
+#sudo restart network-manager
+sudo killall wpa_supplicant
