74a758e49cee19f5906928a95e27718cc2919ed1
Ethan Gaebel <egaebel@vt.edu>
2016-02-28 18:23:55 -0500

    changes to tweak setup monitor

--- injection-monitor/setup_monitor_csi.sh
+++ injection-monitor/setup_monitor_csi.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/sudo /bin/bash
+SLEEP_TIME=2
 WLAN_INTERFACE=$3
+echo "Bringing $WLAN_INTERFACE down....."
+ifconfig $WLAN_INTERFACE down
+sleep $SLEEP_TIME
 echo "Loading up firmware......"
 modprobe -r iwlwifi mac80211 cfg80211
-modprobe iwlwifi connector_log=0x1
+#modprobe iwlwifi connector_log=0x1
+modprobe iwlwifi connector_log=0x5
 echo "Setting $WLAN_INTERFACE into monitor mode....Looping until success..."
 # Setup monitor mode, loop until it works
 iwconfig $WLAN_INTERFACE mode monitor 2>/dev/null 1>/dev/null
@@ -10,7 +15,10 @@ while [ $? -ne 0 ]
 do
     iwconfig $WLAN_INTERFACE mode monitor 2>/dev/null 1>/dev/null
 done
+sleep $SLEEP_TIME
 echo "Bringing $WLAN_INTERFACE up......."
 ifconfig $WLAN_INTERFACE up
+sleep $SLEEP_TIME
+sleep $SLEEP_TIME
 echo "Setting channel to monitor on $WLAN_INTERFACE to $1 $2" 
-iw $WLAN_INTERFACE set channel $1 $2
+iw dev $WLAN_INTERFACE set channel $1 $2
