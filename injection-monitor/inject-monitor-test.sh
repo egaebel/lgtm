3e72131e8e8ebc51b38abe9f1090a3e5ac915214
Ethan Gaebel <egaebel@vt.edu>
2016-03-06 16:34:27 -0500

    lots of progress towards full-back-and-forth communications....still issues with reading results. Problems may exist in log_to_file... Also updated all commands to use only 'iw' and 'ip' instead of iwconfig and ifconfig, which are both deprecated

--- injection-monitor/inject-monitor-test.sh
+++ injection-monitor/inject-monitor-test.sh
@@ -8,33 +8,30 @@ SWITCH_WAIT_TIME=5
 
 injection_mode () {
     echo "Setting $WLAN_INTERFACE into injection mode................................"
-    ifconfig $WLAN_INTERFACE down
-    #sleep $SLEEP_TIME
+    ip link set $WLAN_INTERFACE down
     echo "Deleting mon0...................................................."
     iw dev mon0 del 2>/dev/null 1>/dev/null
     echo "Bringing up firmware............................................."
     modprobe -r iwlwifi mac80211 cfg80211
     modprobe iwlwifi debug=0x40000
-    #echo "Changing $WLAN_INTERFACE mode to ad-hoc.........................."
-    #iwconfig $WLAN_INTERFACE mode ad-hoc
     echo "Running ifconfig on $WLAN_INTERFACE, looping until success................."  
-    ifconfig $WLAN_INTERFACE 2>/dev/null 1>/dev/null
+    ip link show $WLAN_INTERFACE 2>/dev/null 1>/dev/null
     while [ $? -ne 0 ]
     do
-        ifconfig $WLAN_INTERFACE 2>/dev/null 1>/dev/null
+        ip link show $WLAN_INTERFACE 2>/dev/null 1>/dev/null
         sleep $SLEEP_TIME
     done
     echo "Adding monitor to $WLAN_INTERFACE ..............................."
     iw dev $WLAN_INTERFACE interface add mon0 type monitor
-    #sleep $SLEEP_TIME
     echo "Bringing mon0 up................................................."
-    ifconfig mon0 up
+    ip link set mon0 up
     sleep $SLEEP_TIME
-    echo "Killing the notorious wpa_supplicant............................."
-    killall wpa_supplicant
     sleep $SLEEP_TIME
+    #echo "Killing the notorious wpa_supplicant............................."
+    #killall wpa_supplicant
+    #sleep $SLEEP_TIME
     echo "Killing default wireless interface, wlan0........................"
-    ifconfig wlan0 down
+    ip link set wlan0 down
     sleep $SLEEP_TIME
     echo "Setting channel on mon0 to $channel_number $channel_type ................................"
     iw dev mon0 set channel $channel_number $channel_type
@@ -44,48 +41,55 @@ injection_mode () {
 }
 
 monitor_mode () {
-    #echo "Deleting mon0...................................................."
-    #iw dev mon0 del 2>/dev/null 1>/dev/null
-    echo "Bringing down $WLAN_INTERFACE ..................................."
-    ifconfig $WLAN_INTERFACE down
-    sleep $SLEEP_TIME
+    echo "Setting $WLAN_INTERFACE into monitor mode.................................."
     echo "Bringing up firmware............................................."
     modprobe -r iwlwifi mac80211 cfg80211
     modprobe iwlwifi connector_log=0x5
+    echo "Bringing down $WLAN_INTERFACE ..................................."
+    ip link set $WLAN_INTERFACE down 2>/dev/null 1>/dev/null
     echo "Setting $WLAN_INTERFACE into monitor mode........................"
     # Setup monitor mode, loop until it works
-    iwconfig $WLAN_INTERFACE mode monitor 2>/dev/null 1>/dev/null
-    echo "|| $! ||"
-    while [[ $! -ne 0 ]]
-    do
-        iwconfig $WLAN_INTERFACE mode monitor 2>/dev/null 1>/dev/null
-        echo "|| $! ||"
+    iw dev $WLAN_INTERFACE set type monitor 2>/dev/null 1>/dev/null
+    mode_change=$?
+    while [ $mode_change -ne 0 ]; do
+        ip link set $WLAN_INTERFACE down 2>/dev/null 1>/dev/null
+        iw dev $WLAN_INTERFACE set type monitor 
+#2>/dev/null 1>/dev/null
+        mode_change=$?
+        echo mode change: $mode_change
     done
-    sleep $SLEEP_TIME
+    #sleep $SLEEP_TIME
     echo "Bringing $WLAN_INTERFACE up......................................"
-    ifconfig $WLAN_INTERFACE up
-    sleep $SLEEP_TIME
-    sleep $SLEEP_TIME
-    echo "Killing the notorious wpa_supplicant............................."
-    killall wpa_supplicant
-    sleep $SLEEP_TIME
+    ip link set $WLAN_INTERFACE up
+    wlan_interface_up=$(ip link show up | grep $WLAN_INTERFACE | wc -l)
+    while [ $wlan_interface_up -ne 1 ]
+    do
+        ip link set $WLAN_INTERFACE up
+        wlan_interface_up=$(ip link show up | grep $WLAN_INTERFACE | wc -l)
+    done
+    #sleep $SLEEP_TIME
+    #sleep $SLEEP_TIME
+    #echo "Killing the notorious wpa_supplicant............................."
+    #killall wpa_supplicant
+    #sleep $SLEEP_TIME
     echo "Killing default wireless interface, wlan0........................"
-    ifconfig wlan0 down
-    sleep $SLEEP_TIME
+    ip link set wlan0 down
+    #wlan0_up=$(ip link show up | grep wlan0 | wc -l)
+    #while [ $wlan0_up -gt 0 ]
+    #do
+    #    ip link set wlan0 down
+    #    wlan0_up=$(ip link show up | grep wlan0 | wc -l)
+    #done
+    #sleep $SLEEP_TIME
     echo "Setting channel to monitor on $WLAN_INTERFACE to $channel_number $channel_type .........." 
     iw dev $WLAN_INTERFACE set channel $channel_number $channel_type
     echo "Monitor mode active!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 }
 
-
-#echo "Bringing up firmware............................................."
-#modprobe -r iwlwifi mac80211 cfg80211
-#modprobe iwlwifi debug=0x40000
-#sleep $SLEEP_TIME
-
 monitor_mode
 
 echo "Waiting for LGTM initiation......................................"
+rm lgtm-monitor.dat
 ./log_to_file.sh lgtm-monitor.dat &
 
 # Wait for key press or special token to appear in lgtm-monitor.dat
@@ -105,7 +109,7 @@ done
 if [[ $input == 'l' ]]
 then
     echo "Initiating LGTM protocol....................................."
-    pkill log_to_file.sh
+    pkill log_to_file
     # Sleep for 5 seconds to ensure other party has switched into monitor mode
     sleep $SWITCH_WAIT_TIME
     # undo monitor mode settings....(which are what exactly?)
@@ -113,7 +117,7 @@ then
     injection_mode
     # Send "begin-lgtm-protocol", TODO: later this will include a public key
     echo begin-lgtm-protocol > .begin-lgtm-protocol
-    ./packets_from_file two-way-test-file #.begin-lgtm-protocol
+    ./packets_from_file .begin-lgtm-protocol
     rm .begin-lgtm-protocol
     # Switch to monitor mode
     monitor_mode
@@ -121,19 +125,25 @@ then
     rm lgtm-monitor.dat
     ./log_to_file.sh lgtm-monitor.dat &
     lgtm_ack=0
-    while [[ $lgtm_ack < 1 ]]
+    while [[ $lgtm_ack -lt 1 ]]
     do
         # Receive ack + params
         lgtm_ack=$(cat lgtm-monitor.dat | wc -l)
+        echo lgtm_ack: $lgtm_ack
+        sleep $SLEEP_TIME
+        sleep $SLEEP_TIME
+        sleep $SLEEP_TIME
     done
+    pkill log_to_file
     # Sleep for 5 seconds to ensure other party has switched into monitor mode....
     sleep $SWITCH_WAIT_TIME
     # Switch to injection mode
     injection_mode
     # Send facial recognition params
     echo second-level-lgtm-protocol > .lgtm-protocol-continued
-    ./packets_from_file .lgtm-protocol-continued 1
+    ./packets_from_file .lgtm-protocol-continued
     # Done!
+    echo "LGTM COMPLETE!"
 fi
 
 # Token received from other party to initiate LGTM
@@ -154,10 +164,12 @@ then
     rm lgtm-monitor.dat
     ./log_to_file.sh lgtm-monitor.dat &
     lgtm_ack=0
-    while [[ $lgtm_ack < 1 ]]
+    while [[ $lgtm_ack -lt 1 ]]
     do
         # Receive ack + params
         lgtm_ack=$(cat lgtm-monitor.dat | wc -l)
+        sleep $SLEEP_TIME
     done
     # Done!
+    echo "LGTM COMPLETE!"
 fi
