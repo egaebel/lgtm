c9bfd6c7a56495332bd0737887d760962e236a75
Ethan Gaebel <egaebel@vt.edu>
2016-03-21 02:32:07 -0400

    Added lgtm_face_recognition. Right now it's a cleaned up version of facerec video with more arguments.

---
+++ facial-recognition/openCV/lgtm-recognition/lgtm_face_recognition.cpp
@@ -0,0 +1,337 @@
+/**
+ * The MIT License (MIT)
+ * Copyright (c) 2016 Ethan Gaebel <egaebel@vt.edu>
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a 
+ * copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included 
+ * in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+ * OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+/**
+ * The license below covers the code in int main as well as the readCsv function.
+ *
+ * The prior MIT license covers any modifications to that code chunk and all of
+ * the code besides the chunks indicated above.
+ */ 
+
+/**
+ * Copyright (c) 2011. Philipp Wagner <bytefish[at]gmx[dot]de>.
+ * Released to public domain under terms of the BSD Simplified license.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above copyright
+ *     notice, this list of conditions and the following disclaimer in the
+ *     documentation and/or other materials provided with the distribution.
+ *   * Neither the name of the organization nor the names of its contributors
+ *     may be used to endorse or promote products derived from this software
+ *     without specific prior written permission.
+ *
+ *   See <http://www.opensource.org/licenses/bsd-license>
+ */
+
+
+// Switch between Local Binary Patterns Histograms(2), Eigenfaces (1), and Fisherfaces (0)
+#define FACIAL_RECOGNITION_MODEL 2
+
+#include <opencv2/core/core.hpp>
+#include <opencv2/face.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+#include <chrono>
+#include <ctime>
+
+#include <cmath>
+
+using namespace cv;
+using namespace std;
+
+// Function Headers---------------------------------------------------------------------------------
+static void readCsv(const string& fileName, vector<Mat>& images, vector<int>& labels);
+static void drawTargettingLines(int &frameWidth, int &frameHeight, Mat &frame);
+static void findAngleBounds(int &frameWidth, int &frameHeight, Rect &faceRectangle, 
+        double &leftSideAngle, double &rightSideAngle, double &topAngle, double &bottomAngle);
+
+/**
+ *
+ */
+int main(int argc, const char *argv[]) {
+    // Validate input.
+    if (argc < 4) {
+        cout << "usage: " << argv[0] 
+                << " </path/to/haarCascade> </path/to/csv.ext> <device id>"
+                << " <face id> <angles of arrival>" << endl;
+        cout << "\t </path/to/haarCascade> -- Path to the Haar Cascade for face detection." 
+                << endl;
+        cout << "\t </path/to/csv.ext> -- Path to the CSV file with the face database." << endl;
+        cout << "\t <device id> -- The webcam device id to grab frames from." << endl;
+        cout << "\t <face id> -- The identification number of the face we WANT to recognize" 
+                << "for LGTM. This is the number " << endl;
+        cout << "\t <angles of arrival> -- A space separated sequence of angle of arrivals" << endl;
+        exit(1);
+    }
+
+    // Parse inputs
+    string haarCascadeFileName = string(argv[1]);
+    string csvFileName = string(argv[2]);
+    int deviceId = atoi(argv[3]);
+    int faceId = atoi(argv[4]);
+
+    // TODO: make sure I'm removing this....
+    /*
+    string trainedClassifierPath;
+    if (argc == 5) {
+        cout << "Reading trainedClassifierPath from input...." << endl;
+        trainedClassifierPath = string(argv[4]);
+        cout << "Read trainedClassifierPath as: " << trainedClassifierPath << endl;
+    }
+    */
+
+    vector<Mat> images;
+    vector<int> labels;
+
+    try {
+        readCsv(csvFileName, images, labels);
+    } catch (cv::Exception& e) {
+        cerr << "Error opening file \"" << csvFileName << "\". Reason: " << e.msg << endl;
+        exit(1);
+    }
+
+    // TODO: cleaning and maybe removing?
+    int imgWidth = 168;//images[0].cols;
+    int imgHeight = 168;//images[0].rows;
+
+    // Create a FaceRecognizer and train it on the given images:
+    Ptr<face::FaceRecognizer> model;
+    switch (FACIAL_RECOGNITION_MODEL) {
+        case 0:
+            {
+                cout << "Using Fisherfaces" << endl;
+                double threshold = 2200.0;
+                model = face::createFisherFaceRecognizer(0, threshold);
+            }
+            break;
+        case 1:
+            {
+                cout << "Using Eigenfaces" << endl;
+                double threshold = 7250.0;
+                model = face::createEigenFaceRecognizer(0, threshold);
+            }
+            break;
+        case 2:
+            {
+                cout << "Using Local Binary Pattern Histograms" << endl;
+                int radius = 10;
+                int neighbors = 8;
+                int gridX = 4;
+                int gridY = 4;
+                double threshold = 25.0;
+                model = face::createLBPHFaceRecognizer(radius, neighbors, gridX, gridY, threshold);
+            }
+            break;
+    }
+
+    // Load model if a path to a pretrained model was passed
+    cout << "Starting training..." << endl;
+    // Time training...
+    chrono::time_point<std::chrono::system_clock> start, end;
+    start = std::chrono::system_clock::now();
+    
+    model->train(images, labels);
+    model->save("new-facial-recognition-model");
+
+    end = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds = end - start;
+    std::time_t end_time = std::chrono::system_clock::to_time_t(end);
+    cout << "Done training! Took " << elapsed_seconds.count() << " seconds" << endl;
+   
+    // Much of the code below was adapted from the wonderful tutorials in the OpenCV documentation
+    // In particular, the tutorial at: 
+    // http://docs.opencv.org/3.0-beta/modules/face/doc/facerec/tutorial/facerec_video_recognition.html
+    // Load HaarCascade from file
+    CascadeClassifier haarCascade;
+    haarCascade.load(haarCascadeFileName);
+    
+    // Get a handle to the Video device:
+    VideoCapture cap(deviceId);
+    // Check if we can use this device at all:
+    if(!cap.isOpened()) {
+        cerr << "Capture Device ID " << deviceId << "cannot be opened." << endl;
+        return -1;
+    }
+    int capFrameWidth = cap.get(CV_CAP_PROP_FRAME_WIDTH);
+    int capFrameHeight = cap.get(CV_CAP_PROP_FRAME_HEIGHT);
+
+    try {
+        // Holds the current frame from the Video device:
+        Mat frame;
+        for(;;) {
+            cap >> frame;
+            // Clone the current frame:
+            Mat original = frame.clone();
+            Mat gray;
+            cvtColor(original, gray, CV_BGR2GRAY);
+            // Find the faces in the frame:
+            vector< Rect_<int> > faces;
+            haarCascade.detectMultiScale(gray, faces);
+            // Check each face detected in the frame by the HaarCascade classifier 
+            // for facial recognition
+            for(int i = 0; i < faces.size(); i++) {
+                Rect curFace = faces[i];
+                Mat face = gray(curFace);
+                Mat resizedFace;
+                cv::resize(face, resizedFace, Size(imgWidth, imgHeight), 1.0, 1.0, INTER_CUBIC);
+
+                // Predict what this face's ID is
+                int prediction = -1;
+                double confidence = 0.0;
+                model->predict(resizedFace, prediction, confidence);
+
+                // If the prediction is the face we are looking for.
+                if (prediction == faceId) {
+                    rectangle(original, curFace, CV_RGB(0, 255,0), 1);
+                    // Create the text we will annotate the box with:
+                    string boxPredictionText = format("Prediction = %d", prediction);
+                    string boxConfidenceText = format("With confidence: %g", confidence);
+                    // Calculate the position for annotated text (make sure we don't
+                    // put illegal values in there):
+                    // TODO: See below, 10 was the original
+                    int predictionPosX = std::max(curFace.tl().x - 25, 0);
+                    int predictionPosY = std::max(curFace.tl().y - 25, 0);
+                    int confidencePosX = std::max(curFace.tl().x - 10, 0);
+                    int confidencePosY = std::max(curFace.tl().y - 10, 0);
+                    // And now put it into the image:
+                    putText(original, boxPredictionText, Point(predictionPosX, predictionPosY), 
+                            FONT_HERSHEY_PLAIN, 1.0, CV_RGB(0, 255, 0), 2.0);
+                    putText(original, boxConfidenceText, Point(confidencePosX, confidencePosY), 
+                            FONT_HERSHEY_PLAIN, 1.0, CV_RGB(0, 255, 0), 2.0);
+                    double leftSideAngle = -1;
+                    double rightSideAngle = -1;
+                    double topAngle = -1;
+                    double bottomAngle = -1;
+                    findAngleBounds(capFrameWidth, capFrameHeight, curFace, leftSideAngle, rightSideAngle, 
+                            topAngle, bottomAngle);
+                    int thetaPosX = std::max(curFace.tl().x + 10, 0);
+                    int thetaPosY = std::max(curFace.br().y + 10, 0);
+                    int phiPosX = std::max(curFace.tl().x + 25, 0);
+                    int phiPosY = std::max(curFace.br().y + 25, 0);
+                    string boxThetaText = format("Theta 1: %g, Theta2: %g", 
+                            leftSideAngle, rightSideAngle);
+                    string boxPhiText = format("Phi1: %g, Phi2: %g", topAngle, bottomAngle);
+                    putText(original, boxThetaText, Point(thetaPosX, thetaPosY), 
+                            FONT_HERSHEY_PLAIN, 1.0, CV_RGB(0, 255, 0), 2.0);
+                    putText(original, boxPhiText, Point(phiPosX, phiPosY), 
+                            FONT_HERSHEY_PLAIN, 1.0, CV_RGB(0, 255, 0), 2.0);
+                }
+            }
+            // Add "targeting" lines
+            drawTargettingLines(capFrameWidth, capFrameHeight, original);
+
+            // Show the result:
+            imshow("face_recognizer", original);
+            // And display it:
+            int key = waitKey(20);
+            // Exit this loop on escape OR space:
+            if(key == 27 || key == 32) {
+                break;
+            } else if (key != -1) {
+                cout << "Keydown was: " << key 
+                        << " Need to press esc or space to exit loop!" << endl;
+            }
+        }
+    } catch(Exception e) {
+        cap.release();
+    }
+    cap.release();
+    return 0;
+}
+
+static void findAngleBounds(int &frameWidth, int &frameHeight, Rect &faceRectangle, 
+        double &leftSideAngle, double &rightSideAngle, double &topAngle, double &bottomAngle) {
+    // Get points at the two bottom corners of the rectangle
+    int x1 = faceRectangle.tl().x - frameWidth / 2;
+    int x2 = faceRectangle.br().x - frameWidth / 2;
+    int y1 = faceRectangle.tl().y - frameHeight / 2;
+    int y2 = faceRectangle.br().y - frameHeight / 2;
+
+    // Compute theta1 and theta2 (leftSideAngle and rightSideAngle)
+    int z = 500;
+    // Convert to spherical
+    double r1 = sqrt(x1 * x1 + y1 * y1 + z * z);
+    double r2 = sqrt(x2 * x2 + y1 * y1 + z * z);
+    leftSideAngle = acos(z / r1) * (180 / M_PI);
+    rightSideAngle = acos(z / r2) * (180 / M_PI);
+    // Convert to -90 - +90 range
+    leftSideAngle -= 90;
+    rightSideAngle -= 90;
+
+    // Compute phi1 and phi2 (topAngle and bottomAngle)
+    topAngle = atan(y1 / x1) * (180 / M_PI);
+    bottomAngle = atan(y2 / x1) * (180 / M_PI);
+}
+
+static void drawTargettingLines(int &frameWidth, int &frameHeight, Mat &frame) {
+    line(frame, Point(0, frameHeight / 2), Point(frameWidth, frameHeight / 2), CV_RGB(0, 255,0), 1);
+    int topY = frameHeight / 2 - 50;
+    int bottomY = frameHeight / 2 + 50;
+    double x;
+    string angleString;
+    for (int i = 0; i <= 60; i+=10) {
+        x = 739 * sin(i * (M_PI) / 180);
+        cout << x << endl;
+        line(frame, Point(x, topY), Point(x, bottomY), CV_RGB(0, 255,0), 1);
+        angleString = format("%d", (i));
+        putText(frame, angleString, Point(x - 5, bottomY + 15), FONT_HERSHEY_PLAIN, 1.0, 
+                CV_RGB(0, 255, 0), 2.0);    
+    }
+}
+
+/**
+ * Read images and labels from the csv file specified by fileName.
+ */
+static void readCsv(const string& fileName, vector<Mat>& images, vector<int>& labels) {
+    const char separator=';';
+    std::ifstream file(fileName.c_str(), ifstream::in);
+    if (!file) {
+        string error_message = "No valid input file was given, please check the given fileName.";
+        CV_Error(CV_StsBadArg, error_message);
+    }
+    string line, path, classlabel;
+    Mat readImage;
+    while (getline(file, line)) {
+        stringstream liness(line);
+        getline(liness, path, separator);
+        getline(liness, classlabel);
+        if(!path.empty() && !classlabel.empty()) {
+            readImage = imread(path, 0);
+            if (readImage.empty()) {
+                cerr << "Error opening image file at path: \"" << path << endl;
+                continue;
+            }
+            images.push_back(readImage);
+            labels.push_back(atoi(classlabel.c_str()));
+        }
+    }
+}
