c7e76052a5c0c8039c958bb064fcd8feceb9a099
Ethan Gaebel <egaebel@vt.edu>
2016-03-21 16:37:38 -0400

    Added license and comments

--- injection-monitor/full-lgtm.sh
+++ injection-monitor/full-lgtm.sh
@@ -1,14 +1,39 @@
 #!/usr/bin/sudo /bin/bash
 
+##
+# The MIT License (MIT)
+# Copyright (c) 2016 Ethan Gaebel <egaebel@vt.edu>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a 
+# copy of this software and associated documentation files (the "Software"), 
+# to deal in the Software without restriction, including without limitation 
+# the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+# and/or sell copies of the Software, and to permit persons to whom the 
+# Software is furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included 
+# in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+# DEALINGS IN THE SOFTWARE.
+
+# Parameters----------------------------------------------------------------------------------------
 channel_number=$1
 channel_type=$2
 wlan_interface=$3
 facial_recognition_file=$4
 
+# Constants-----------------------------------------------------------------------------------------
 SLEEP_TIME=2
 SWITCH_WAIT_TIME=5
 PACKET_DELAY=0
 
+# Functions-----------------------------------------------------------------------------------------
 injection_mode () {
     echo "Switching $wlan_interface to inject........................................"
     ip link set $wlan_interface down
@@ -98,6 +123,7 @@ monitor_mode () {
     echo "Monitor mode active!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 }
 
+# Main code-----------------------------------------------------------------------------------------
 pkill log_to_file
 monitor_mode
 
