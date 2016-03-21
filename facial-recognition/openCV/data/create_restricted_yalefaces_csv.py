2477204816f260103fe5cb6e617f1ac08bf241b7
Ethan Gaebel <egaebel@vt.edu>
2016-03-21 04:46:19 -0400

    Created restricted yalefaces create csv script so that cetain faces can easily be excluded from training.

---
+++ facial-recognition/openCV/data/create_restricted_yalefaces_csv.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python
+
+import sys
+import os.path
+
+# This is a tiny script to help you creating a CSV file from a face
+# database with a similar hierarchie:
+# 
+#  philipp@mango:~/facerec/data/at$ tree
+#  .
+#  |-- README
+#  |-- s1
+#  |   |-- 1.pgm
+#  |   |-- ...
+#  |   |-- 10.pgm
+#  |-- s2
+#  |   |-- 1.pgm
+#  |   |-- ...
+#  |   |-- 10.pgm
+#  ...
+#  |-- s40
+#  |   |-- 1.pgm
+#  |   |-- ...
+#  |   |-- 10.pgm
+#
+
+if __name__ == "__main__":
+    
+    if len(sys.argv) < 2:
+        print("usage: create_csv <base_path> <Yaleface id numbers to exclude>")
+        sys.exit(1)
+    
+    BASE_PATH = sys.argv[1]
+    SEPARATOR = ";"
+    exclude_set = {}
+    for arg in sys.argv[2:]:
+        exclude_set[int(arg)] = True
+    print(exclude_set)
+    for dirname, dirnames, filenames in os.walk(BASE_PATH):
+        for subdirname in dirnames:
+            label = -1
+            subject_path = os.path.join(dirname, subdirname)
+            for filename in os.listdir(subject_path):
+                label = filename[len("subject"):]
+                period_index = label.find(".")
+                label = label[:period_index]
+                label = int(label)
+                if label not in exclude_set:
+                    abs_path = "%s/%s" % (subject_path, filename)
+                    print("%s%s%d" % (abs_path, SEPARATOR, label))
