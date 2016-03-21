76c89b075673205dc41800796c40ca92f0852c17
Ethan Gaebel <egaebel@vt.edu>
2016-03-21 06:12:43 -0400

    Added bash scripts to run & test full lgtm facial recognition program

---
+++ facial-recognition/openCV/lgtm-recognition/run_lgtm_facial_recognition.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./lgtm_facial_recognition haarcascades/haarcascade_frontalface_default.xml data_files.csv 1 "$@"
---
+++ facial-recognition/openCV/lgtm-recognition/test_lgtm_facial_recognition.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+./lgtm_facial_recognition haarcascades/haarcascade_frontalface_default.xml yalefaces.csv 1 9 -20 10
