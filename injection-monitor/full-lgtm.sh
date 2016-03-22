48c3a3ff3dda9e31e4f1ebc01f28054aa8e9b790
Ethan Gaebel <egaebel@vt.edu>
2016-03-22 00:54:38 -0400

    MODULARIZATION! Made functions for sending, receiving and parsing, and matching up locations

--- injection-monitor/full-lgtm.sh
+++ injection-monitor/full-lgtm.sh
@@ -129,6 +129,90 @@ monitor_mode () {
     echo "Monitor mode active!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
 }
 
+send_facial_recognition_params () {
+    echo "Sending 'facial recognition params'.............................."
+    # Setup Injection mode
+    injection_mode
+    # Sleep for 5 seconds to ensure other party has switched into monitor mode....
+    sleep $SWITCH_WAIT_TIME
+    # Send acknowledgement + facial recognition params, TODO: later this will include a public key
+    # Send facial recognition params
+    rm .lgtm-facial-recognition-params
+    echo $FACIAL_RECOGNITION_HEADER > .lgtm-facial-recognition-params
+    cat $facial_recognition_file >> .lgtm-facial-recognition-params
+    echo $FACIAL_RECOGNITION_FOOTER >> .lgtm-facial-recognition-params
+    ./packets-from-file/packets_from_file .lgtm-facial-recognition-params 1
+    echo "Sent 'facial recognition params'!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+}
+
+receive_and_process_facial_recognition_params () {
+    echo "Receiving and processing 'facial recognition params'............."
+    # Switch to monitor mode
+    monitor_mode
+
+    # Listen for facial recognition parameters, TODO: later it will be ack + recog params + public key
+    echo "Awaiting 'facial recognition params'............................."
+    rm .lgtm-monitor.dat
+    ./log-to-file/log_to_file .lgtm-monitor.dat &
+    lgtm_ack=0
+    while [ $lgtm_ack -lt 1 ]; do
+        # Receive ack + params
+        lgtm_ack=$(cat .lgtm-monitor.dat | grep $FACIAL_RECOGNITION_FOOTER | wc -l)
+    done
+    pkill log_to_file
+    echo "Received 'facial recognition params'!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+
+    # Localize wireless signal
+    echo "Localizing signal source........................................."
+    chmod 644 .lgtm-monitor.dat
+    logged_on_user=$(who | head -n1 | awk '{print $1;}')
+    sudo -u $logged_on_user matlab -nojvm -nodisplay -nosplash -r "run('../csi-code/spotfi.m'), exit"
+    echo "Successfully localized signal source!"
+
+    # Extract data from mpdus in packets
+    echo "Extracting data from received packets............................"
+    # Extract data on facial recognition params from received data
+    sudo -u $logged_on_user matlab -nojvm -nodisplay -nosplash -r "run('read_mpdu_file.m'), exit"    
+    echo "Data extracted!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+}
+
+compare_wireless_location_with_face_location () {
+    echo "Checking for face/signal overlap................................."
+    # Strip off $FACIAL_RECOGNITION_HEADER, $FACIAL_RECOGNITION_FOOTER, and anything before or after
+    # Plus one for the string terminator ('\0')
+    num_header_bytes=$((${#FACIAL_RECOGNITION_HEADER} + 1))
+    cat .lgtm-received-facial-recognition-params | dd bs=1 skip=$num_header_bytes > .lgtm-received-facial-recognition-params--no-header
+    byte_offset=$(cat .lgtm-received-facial-recognition-params--no-header | grep --byte-offset --only-matching --text $FACIAL_RECOGNITION_FOOTER | grep --only-matching [0-9]*)
+    cat .lgtm-received-facial-recognition-params--no-header | dd bs=1 count=$byte_offset > .lgtm-received-facial-recognition-params--no-header--no-footer
+    
+    # Extract files from tar archive    
+    facial_recognition_params_folder=$(tar xvf .lgtm-received-facial-recognition-params--no-header--no-footer | head -n 1)
+    echo "facial_recognition_params_folder: " $facial_recognition_params_folder
+    mv $facial_recognition_params_folder .lgtm-facial-recognition-training-photos
+
+    # Generate csv file with paths to images for training and labels
+    ./create_yalefaces_csv.py .lgtm-facial-recognition-training-photos
+    # Create CSV file for just-received photos
+    ./create_yalefaces_csv.py .lgtm-facial-recognition-training-photos > .lgtm-facial-recognition-training-photo-paths.csv
+
+    # Grab the label from the first entry (they're assumed to all be the same)
+    # The label is from after the semi-colon to the end of the line
+    face_id=$(cat .lgtm-facial-recognition-training-photo-paths.csv | head -n1 | grep -o ";.*$" | cut -c 2-)
+    top_aoas=$(cat .lgtm-top-aoas)
+
+    # Change folder to run facial recognition program
+    old_dir=$(pwd)
+    cd ../facial-recognition/lgtm-recognition/
+
+    # Run facial recognition
+    ./run_lgtm_facial_recognition.sh $webcam_id $old_dir/.lgtm-facial-recognition-training-photo-paths.csv $face_id $top_aoas
+
+    # Echo exit status of LGTM facial recognition command
+    echo $?
+    cd $old_dir
+    pwd
+}
+
 # Main code-----------------------------------------------------------------------------------------
 pkill log_to_file
 monitor_mode
@@ -151,77 +235,22 @@ done
 if [[ $input == 'l' ]]; then
     echo "Initiating LGTM protocol........................................."
     pkill log_to_file
+
     # Sleep for 5 seconds to ensure other party has switched into monitor mode
     sleep $SWITCH_WAIT_TIME
-    # undo monitor mode settings....(which are what exactly?)
+
     # Setup Injection mode
     injection_mode
+
     # Send "begin-lgtm-protocol", TODO: later this will include a public key
     rm .lgtm-begin-protocol
     echo $LGTM_BEGIN_TOKEN > .lgtm-begin-protocol
     ./packets-from-file/packets_from_file .lgtm-begin-protocol 1 $PACKET_DELAY
-    # Switch to monitor mode
-    monitor_mode
-    # Wait for acknowledgement + facial recognition params, TODO: later it will be ack + recog params + public key
-    echo "Awaiting 'facial recognition params'!"
-    rm .lgtm-monitor.dat
-    ./log-to-file/log_to_file .lgtm-monitor.dat &
-    lgtm_ack=0
-    while [ $lgtm_ack -lt 1 ]; do
-        # Receive ack + params
-        lgtm_ack=$(cat .lgtm-monitor.dat | grep $FACIAL_RECOGNITION_FOOTER | wc -l)
-    done
-    pkill log_to_file
-    echo "Received 'facial recognition params'!"
-    echo "Localizing signal source!"
-    chmod 644 .lgtm-monitor.dat
-
-    logged_on_user=$(who | head -n1 | awk '{print $1;}')
-    sudo -u $logged_on_user matlab -nojvm -nodisplay -nosplash -r "run('../csi-code/spotfi.m'), exit"
-    echo "Successfully localized signal source!"
-
-    echo "Extracting data from received packets............................"
-    # Extract data on facial recognition params from received data
-    sudo -u $logged_on_user matlab -nojvm -nodisplay -nosplash -r "run('read_mpdu_file.m'), exit"    
-    echo "Data extracted!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
     
-    # Sleep for 5 seconds to ensure other party has switched into monitor mode.... TODO: Shorten or remove this....
-    sleep $SWITCH_WAIT_TIME
-    # Switch to injection mode
-    injection_mode
+    receive_and_process_facial_recognition_params()
+    send_facial_recognition_params()
+    compare_wireless_location_with_face_location()
 
-    # Send facial recognition params
-    echo "Sending 'facial recognition params'!"
-    rm .lgtm-facial-recognition-params
-    echo $FACIAL_RECOGNITION_HEADER > .lgtm-facial-recognition-params
-    cat $facial_recognition_file >> .lgtm-facial-recognition-params
-    echo $FACIAL_RECOGNITION_FOOTER >> .lgtm-facial-recognition-params
-    ./packets-from-file/packets_from_file .lgtm-facial-recognition-params 1
-    
-    echo "Checking for face/signal overlap................................."
-    # Strip off $FACIAL_RECOGNITION_FOOTER and $FACIAL_RECOGNITION_HEADER and anything before and after
-    #cat .lgtm-received-facial-recognition-params | cut -c $((${#FACIAL_RECOGNITION_HEADER} + 1))- > .lgtm-received-facial-recognition-params--no-header
-    # Plus one for the string terminator
-    num_header_bytes=$((${#FACIAL_RECOGNITION_HEADER} + 1))
-    cat .lgtm-received-facial-recognition-params | dd bs=1 skip=$num_header_bytes > .lgtm-received-facial-recognition-params--no-header
-    byte_offset=$(cat .lgtm-received-facial-recognition-params--no-header | grep --byte-offset --only-matching --text $FACIAL_RECOGNITION_FOOTER | grep --only-matching [0-9]*)
-    cat .lgtm-received-facial-recognition-params--no-header | dd bs=1 count=$byte_offset > .lgtm-received-facial-recognition-params--no-header--no-footer
-    echo byte offset: $byte_offset
-    facial_recognition_params_folder=$(tar xvf .lgtm-received-facial-recognition-params--no-header--no-footer | head -n 1)
-    echo "facial_recognition_params_folder: " $facial_recognition_params_folder
-    ./create_yalefaces_csv.py $facial_recognition_params_folder
-    # Create CSV file for just-received photos
-    ./create_yalefaces_csv.py $facial_recognition_params_folder > .lgtm-facial-recognition-training-photo-paths.csv
-    # Grab the characters after the semi-colon to the end of the line on the first line
-    face_id=$(cat .lgtm-facial-recognition-training-photo-paths.csv | head -n1 | grep -o ";.*$" | cut -c 2-)
-    top_aoas=$(cat .lgtm-top-aoas)
-    old_dir=$(pwd)
-    cd ../facial-recognition/lgtm-recognition/
-    ./run_lgtm_facial_recognition.sh $webcam_id $old_dir/.lgtm-facial-recognition-training-photo-paths.csv $face_id $top_aoas
-    # Echo exit status of LGTM facial recognition command
-    echo $?
-    cd $old_dir
-    pwd
     # Done!
     echo "LGTM COMPLETE!"
     exit
@@ -230,62 +259,11 @@ fi
 # Token received from other party to initiate LGTM
 if [ $begin_lgtm -gt 0 ]; then
     echo "Other party initiated LGTM protocol.............................."
-    # Setup Injection mode
-    injection_mode
-    # Sleep for 5 seconds to ensure other party has switched into monitor mode....
-    sleep $SWITCH_WAIT_TIME
-    # Send acknowledgement + facial recognition params, TODO: later this will include a public key
-    # Send facial recognition params
-    echo "Sending 'facial recognition params'!"
-    rm .lgtm-facial-recognition-params
-    echo $FACIAL_RECOGNITION_HEADER > .lgtm-facial-recognition-params
-    cat $facial_recognition_file >> .lgtm-facial-recognition-params
-    echo $FACIAL_RECOGNITION_FOOTER >> .lgtm-facial-recognition-params
-    ./packets-from-file/packets_from_file .lgtm-facial-recognition-params 1
-
-    # Setup Monitor mode
-    monitor_mode
-    # Await facial recognition params
-    echo "Awaiting 'facial recognition params'!"
-    rm .lgtm-monitor.dat
-    ./log-to-file/log_to_file .lgtm-monitor.dat &
-    lgtm_ack=0
-    while [[ $lgtm_ack -lt 1 ]]; do
-        # Receive ack + params
-        lgtm_ack=$(cat .lgtm-monitor.dat | grep $FACIAL_RECOGNITION_FOOTER | wc -l)
-    done
-    pkill log_to_file
-    echo "Received 'facial recognition params'!"
-    echo "Localizing signal source!"
-    chmod 644 .lgtm-monitor.dat
-
-    logged_on_user=$(who | head -n1 | awk '{print $1;}')
-    sudo -u $logged_on_user matlab -nojvm -nodisplay -nosplash -r "run('../csi-code/spotfi.m'), exit"
-    echo "Successfully localized signal source!"
-
-    echo "Extracting data from received packets............................"
-    # Extract data on facial recognition params from received data
-    sudo -u $logged_on_user matlab -nojvm -nodisplay -nosplash -r "run('read_mpdu_file.m'), exit"
-    echo "Data extracted!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-
-    echo "Checking for face/signal overlap................................."
-    # Strip off $FACIAL_RECOGNITION_FOOTER and $FACIAL_RECOGNITION_HEADER and anything before and after
-    #cat .lgtm-received-facial-recognition-params | cut -c $((${#FACIAL_RECOGNITION_HEADER} + 1))- > .lgtm-received-facial-recognition-params--no-header
-    # Plus one for the string terminator
-    num_header_bytes=$((${#FACIAL_RECOGNITION_HEADER} + 1))
-    cat .lgtm-received-facial-recognition-params | dd bs=1 skip=$num_header_bytes > .lgtm-received-facial-recognition-params--no-header
-    byte_offset=$(cat .lgtm-received-facial-recognition-params--no-header | grep --byte-offset --only-matching --text $FACIAL_RECOGNITION_FOOTER | grep --only-matching [0-9]*)
-    cat .lgtm-received-facial-recognition-params--no-header | dd bs=1 count=$byte_offset > .lgtm-received-facial-recognition-params--no-header--no-footer
-    #cat .lgtm-received-facial-recognition-params--no-header | rev | cut -c $((${#FACIAL_RECOGNITION_FOOTER} + 1))- | rev > .lgtm-received-facial-recognition-params--no-header--no-footer
-    tar xf .lgtm-received-facial-recognition-params--no-header--no-footer
-    # Create CSV file for just-received photos
-    ./../facial-recognition/lgtm-recognition/create_yalefaces_csv.py .lgtm-received-facial-recognition-params--no-header--no-footer > .lgtm-facial-recognition-training-photo-paths.csv
-    # Grab the characters after the semi-colon to the end of the line on the first line
-    face_id=$(cat .lgtm-facial-recognition-training-photo-paths.csv | head -n1 | grep -o ";.*$" | cut -c 2-)
-    top_aoas=$(cat .lgtm-top-aoas)
-    ./../facial-recognition/lgtm-recognition/run_lgtm_facial_recognition.sh .lgtm-facial-recognition-training-photo-paths.csv $face_id top_aoas
-    # Echo exit status of LGTM facial recognition command
-    echo $?
+    
+    send_facial_recognition_params()
+    receive_and_process_facial_recognition_params()
+    compare_wireless_location_with_face_location()
+    
     # Done!
     echo "LGTM COMPLETE!"
     exit
