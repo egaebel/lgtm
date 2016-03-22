fda3426a9ded254a99be3650541deba40bea449e
Ethan Gaebel <egaebel@vt.edu>
2016-03-22 02:15:22 -0400

    Added licenses.

--- injection-monitor/read_mpdu_file.m
+++ injection-monitor/read_mpdu_file.m
@@ -1,3 +1,26 @@
+%%
+% The MIT License (MIT)
+% Copyright (c) 2016 Ethan Gaebel <egaebel@vt.edu>
+% 
+% Permission is hereby granted, free of charge, to any person obtaining a 
+% copy of this software and associated documentation files (the "Software"), 
+% to deal in the Software without restriction, including without limitation 
+% the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+% and/or sell copies of the Software, and to permit persons to whom the 
+% Software is furnished to do so, subject to the following conditions:
+% 
+% The above copyright notice and this permission notice shall be included 
+% in all copies or substantial portions of the Software.
+% 
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
+% OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+% FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+% DEALINGS IN THE SOFTWARE.
+%
+
 %READ_MPDU_FILE Reads in file of Media Access Control Protocol Data Unit 
 %       (MPDU) logs and outputs the parsed data bytes to a file.
 %   This version uses the *C* version of read_bfee, compiled with
