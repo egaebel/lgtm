dedc4be1d9b7317863f43413132737ce0a6dd918
Ethan G <egaebel@vt.edu>
2016-04-17 15:03:52 -0400

    Added distribute chunks script for sending chunks of data analysis to different nodes on rlogin

---
+++ csi-code/test-code/distribute_chunks.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+my_dir=/home/ugrads/ugrads1/e/egaebel/thesis-project/lgtm/csi-code/test-code
+
+ssh ash -t "$my_dir/run_matlab_data_proc.sh 1"
+ssh beech -t "$my_dir/run_matlab_data_proc.sh 2"
+ssh birch -t "$my_dir/run_matlab_data_proc.sh 3"
+ssh boxelder -t "$my_dir/run_matlab_data_proc.sh 4"
+ssh cedar -t "$my_dir/run_matlab_data_proc.sh 5"
+
+ssh chinkapin -t "$my_dir/run_matlab_data_proc.sh 6"
+ssh dogwood -t "$my_dir/run_matlab_data_proc.sh 7"
+ssh gum -t "$my_dir/run_matlab_data_proc.sh 8"
+ssh hackberry -t "$my_dir/run_matlab_data_proc.sh 9"
+ssh hickory -t "$my_dir/run_matlab_data_proc.sh 10"
+
+ssh hornbeam -t "$my_dir/run_matlab_data_proc.sh 11"
+ssh linden -t "$my_dir/run_matlab_data_proc.sh 12"
+ssh locust -t "$my_dir/run_matlab_data_proc.sh 13"
+ssh maple -t "$my_dir/run_matlab_data_proc.sh 14"
+ssh mulberry -t "$my_dir/run_matlab_data_proc.sh 15"
+
+ssh pawpaw -t "$my_dir/run_matlab_data_proc.sh 16"
+ssh pine -t "$my_dir/run_matlab_data_proc.sh 17"
+ssh sassafras -t "$my_dir/run_matlab_data_proc.sh 18"
+ssh spruce -t "$my_dir/run_matlab_data_proc.sh 19"
+ssh sumac -t "$my_dir/run_matlab_data_proc.sh 20"
+
